0f47176696b6d0924278b0c66c49a182353b132b
Ashmit Yadav <108016803+Ashmit-1131@users.noreply.github.com>
2025-09-23 11:16:45 +0530

    Update apiClient.js

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8082/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://leave-manager-j75y.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
